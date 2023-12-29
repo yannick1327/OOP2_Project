1a76d4b98d334f92e626599f599ffc27f6edcf46
Leon Rausch <leonrausch022@gmail.com>
2023-12-29 21:30:50 +0100

    funktionierende Codebasis für weitere Veränderungen erstellt

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        // Beispielmatrizen
+        int[][] matrixA = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[][] matrixB = {{9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}};
+
+        // Größe der Matrizen
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int rowsB = matrixB.length;
+        int colsB = matrixB[0].length;
+
+        // Ergebnismatrix initialisieren
+        int[][] result = new int[rowsA][colsB];
+
+        // Anzahl der Threads (kann angepasst werden)
+        // Gibt zurück wie viele Threads der Runtime zur Verfügung stehen (normalerweise so viele wie viele Threads die CPU hat bei mir 16)
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        // Multiplikation in Threads aufteilen
+        for (int i = 0; i < rowsA; i++) {
+            final int row = i;
+            executor.submit(() -> multiplyRowByMatrix(matrixA, matrixB, result, row));
+        }
+
+        // Warten, bis alle Threads abgeschlossen sind
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Ergebnis ausgeben
+        printMatrix(result);
+    }
+
+    private static void multiplyRowByMatrix(int[][] matrixA, int[][] matrixB, int[][] result, int row) {
+        for (int colB = 0; colB < matrixB[0].length; colB++) {
+            for (int k = 0; k < matrixA[0].length; k++) {
+                result[row][colB] += matrixA[row][k] * matrixB[k][colB];
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
         }
     }
 }
