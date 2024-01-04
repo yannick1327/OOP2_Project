c30b90b3359669341d4ed286a51a93cefe2a320c
yannick1327 <yannick.diehl2003@gmail.com>
2024-01-04 18:52:19 +0100

    added TaskType enum and Vector class; modified Matrix class

---
+++ src/Enums/Tasktype.java
@@ -0,0 +1,12 @@
+package Enums;
+public enum Tasktype
+{
+    VectorAddidtion,
+    VectorSubstraction,
+    ScalarMultiplication,
+    ScalarProduct,
+    CrossProduct,
+    MatrixAddition,
+    MatrixProduct,
+    Invert2x2Matrix
+}
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,38 @@
-public class Matrix {
+import java.util.List;
 
+public class Matrix
+{
+    private final int _dimensionX;
+    private final int _dimensionY;
+
+    private List<Vector> parts; // vertical separated
+
+    public Matrix(int dimensionX,int dimensionY, Vector firstColumn,Vector secondColumn)
+    {
+        _dimensionX = dimensionX;
+        _dimensionY = dimensionY;
+
+        parts.add(firstColumn);
+        parts.add(secondColumn);
+    }
+
+    public Matrix(int dimensionX,int dimensionY, Vector firstColumn,Vector secondColumn,Vector thirdColumn)
+    {
+        _dimensionX = dimensionX;
+        _dimensionY = dimensionY;
+
+        parts.add(firstColumn);
+        parts.add(secondColumn);
+        parts.add(thirdColumn);
+    }
+
+    public int get_dimensionX()
+    {
+        return _dimensionX;
+    }
+
+    public int get_dimensionY()
+    {
+        return _dimensionY;
+    }
 }
