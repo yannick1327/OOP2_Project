785f083af2f614990e110be8c4d209d2b5474f7c
yannick1327 <yannick.diehl2003@gmail.com>
2024-01-06 20:34:31 +0100

    splited tasktypes for Matrixoperation and Vectoroperations

---
+++ src/Enums/TasktypeVectoroperations.java
@@ -0,0 +1,10 @@
+package Enums;
+
+public enum TasktypeVectoroperations
+{
+    VectorAddidtion, // additon von zwei Vektoren
+    VectorSubstraction, // subtraktion von zwei Vektoren
+    VectorProduct, // zwei Vektoren die einen Vektor ergeben
+    ScalarMultiplication, // ein Vektor und eine Zahl die einen Vektor ergeben
+    ScalarProduct, // zwei Vektoren die eine Zahl ergeben
+}
